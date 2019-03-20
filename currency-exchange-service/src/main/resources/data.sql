e54ddfe2f8efc9f6faa0974c2902425d6a6a203a
Thang <nguyendinhthang192@gmail.com>
2019-03-20 14:26:52 +0700

    update

--- currency-exchange-service/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10001,'USD','VND', 23, 0);
-insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10002,'EUR','VND', 35, 0);
-insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'INR','VND', 12, 0);
