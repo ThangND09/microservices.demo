5ebdb7f6290518fc15f6057583f5c25a7b49d9da
Thang <nguyendinhthang192@gmail.com>
2019-03-22 16:50:07 +0700

    jgbikjgbkjhb

--- currency-client-service/src/main/java/thangnd15/microservices/currencyclientservice/CurrencyClientServiceApplication.java
+++ currency-client-service/src/main/java/thangnd15/microservices/currencyclientservice/CurrencyClientServiceApplication.java
@@ -2,10 +2,12 @@ package thangnd15.microservices.currencyclientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("thangnd15.microservices.currencyclientservice")
+@EnableDiscoveryClient
 public class CurrencyClientServiceApplication {
 
 	public static void main(String[] args) {
